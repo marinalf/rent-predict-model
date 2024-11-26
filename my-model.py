16a512f147576e9b7a513b13f30672c77ce8e4e8
Marina <marinalf@msn.com>
2024-11-26 00:40:52 -0500

    updated model

--- my-model.py
+++ my-model.py
@@ -45,16 +45,10 @@ fig.show()
 
 """
 # Converting categorical into numerical features (Label Encoding)
-data["Area Type"] = data["Area Type"].map({"Super Area": 1, 
-                                           "Carpet Area": 2, 
-                                           "Built Area": 3})
-data["Borough"] = data["Borough"].map({"Queens": 4000, "Brooklyn": 5000, 
-                                 "Manhattan": 6000})
-data["Furnishing Status"] = data["Furnishing Status"].map({"Unfurnished": 0,  
-                                                           "Furnished": 1})
-data["Tenant Preferred"] = data["Tenant Preferred"].map({"Bachelors/Family": 2, 
-                                                         "Bachelors": 1, 
-                                                         "Family": 3})
+data["Area Type"] = data["Area Type"].map({"Super Area": 1, "Carpet Area": 2, "Built Area": 3})
+data["Borough"] = data["Borough"].map({"Queens": 4000, "Brooklyn": 5000, "Manhattan": 6000})
+data["Furnishing Status"] = data["Furnishing Status"].map({"Unfurnished": 0, "Furnished": 1})
+data["Tenant Preferred"] = data["Tenant Preferred"].map({"Bachelors/Family": 2, "Bachelors": 1, "Family": 3})
 
 # Splitting data and test sets
 from sklearn.model_selection import train_test_split
@@ -62,9 +56,7 @@ from sklearn.model_selection import train_test_split
 x = np.array(data[["Rooms", "Size", "Borough", "Furnishing Status"]])
 y = np.array(data[["Rent"]])
 
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, 
-                                                test_size=0.10, 
-                                                random_state=42)
+xtrain, xtest, ytrain, ytest = train_test_split(x, y,test_size=0.10, random_state=42)
 
 # Neural Network
 from keras.models import Sequential # type: ignore
